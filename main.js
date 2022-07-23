30150a1f7f3724d697a1c41ebea865cb9fc0c383
Leandro Santos <leandrooliveira574@gmail.com>
2022-07-23 00:01:54 -0300

    Adicionando nome Empresa

--- main.js
+++ main.js
@@ -6,8 +6,10 @@ module.exports.templateTags = [
     require('./src/gerador/nome'),
     require('./src/gerador/email'),
     require('./src/gerador/telefone'),
-    require('./src/gerador/cep') ,
-    require('./src/gerador/cpf') ,
-    require('./src/gerador/cnpj')
+    require('./src/gerador/cep'),
+    require('./src/gerador/cpf'),
+    require('./src/gerador/cnpj'),
+    require('./src/gerador/nomeEmpresa')
+
 ];
 
