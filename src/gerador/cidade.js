ed26346af3c6d07d727f462af52bfed2c279aed6
Iugor Sette Pereira <iugorsette@gmail.com>
2025-09-03 13:31:25 -0300

    implementando filtro de cidades por estado

--- src/gerador/cidade.js
+++ src/gerador/cidade.js
@@ -4,15 +4,34 @@ const RandomUtils = require('../utils/RandomUtils')
 module.exports = {
   name: 'cidade',
   displayName: 'Cidade',
-  description: 'Gera uma cidade brasileira aleatória',
-  args: [],
-  run(context) {
-    const estado =
-      estadosCidades.estados[
-        RandomUtils.getRandomInt(0, estadosCidades.estados.length - 1)
-      ]
+  description: 'Gera uma cidade brasileira aleatória (pode ser limitada a um estado)',
+  args: [
+    {
+      displayName: 'Estado',
+      description: 'Sigla do estado',
+      type: 'string',
+      defaultValue: '',
+    },
+  ],
+  run(context, estadoSigla) {
+    let estado
+
+    if (estadoSigla && estadoSigla.trim() !== '') {
+      estado = estadosCidades.estados.find(
+        (e) => e.sigla.toLowerCase() === estadoSigla.toLowerCase()
+      )
+    }
+
+    if (!estado) {
+      estado =
+        estadosCidades.estados[
+          RandomUtils.getRandomInt(0, estadosCidades.estados.length - 1)
+        ]
+    }
+
     const cidade =
       estado.cidades[RandomUtils.getRandomInt(0, estado.cidades.length - 1)]
+
     return cidade
   },
 }
